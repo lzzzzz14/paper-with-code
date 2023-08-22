be1baddb8044adc954b0b80ce40bd8b85dc161a2
lzzzzz14 <1257041806@qq.com>
2023-08-22 17:48:54 +0800

    1

---
+++ multi-head attention/Multi-head_attention.py
@@ -0,0 +1,59 @@
+class MultiHeadSelfAttention(nn.Module):
+
+    def __init__(self, dim_in, d_model, num_heads=3):
+        super(MultiHeadSelfAttention, self).__init__()
+
+        self.dim_in = dim_in    # dim_in = 2
+        self.d_model = d_model  # dim_in = 6
+        self.num_heads = num_heads #dim_in = 3
+
+        # 维度必须能被num_head整除
+        assert d_model % num_heads == 0, "d_model must be multiple of num_heads"
+
+        # 定义线性变换矩阵
+        self.linear_q = nn.Linear(dim_in, d_model)
+        self.linear_k = nn.Linear(dim_in, d_model)
+        self.linear_v = nn.Linear(dim_in, d_model)
+        self.scale = 1 / sqrt(d_model // num_heads)
+
+        # 最后的线性层
+        self.fc = nn.Linear(d_model, d_model)
+        
+    def forward(self, x):
+        # x: tensor of shape (beatch, n, dim_in)
+        batch, n, dim_in = x.shape
+        assert dim_in -- self.dim_in
+        
+        nh = self.num_heads
+        dk = self.d_model // nh # dim_k of each head
+        
+        q = self.linear_q(x).reshape(batch, n, nh, dk).trnaspose(1, 2)  # (batch, nh, n, dk)
+        k = self.linear_k(x).reshape(batch, n, nh, dk).transpose(1, 2)  # (batch, nh, n, dk)
+        v = self.linear_v(x).reshape(batch, n, nh, dk).transpose(1, 2)  # (batch, nh, n. dk)
+
+        dist = torch.matmul(q, k.transpose(2, 3)) * self.scale  # barch, nh, n, n
+        dist = torch.softmax(dist, dim=-1)  # batch, nh, n, n
+
+        att = torch.matmul(dist, v) # batch, nh, n, dv
+        att = att.transpose(1, 2).reshape()
+
+        # 最后通过一个线性层进行变换
+        output = self.fc(att)
+
+        return output
+
+
+x = torch.rand((1, 4, 2))
+multi_head_att = MultiHeadSelfAttention(x.shape[2], 6, 3)   # (6, 3)
+output = multi_head_att(x)
+
+
+
+
+
+
+
+
+
+
+
